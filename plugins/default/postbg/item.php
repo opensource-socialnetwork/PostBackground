b0b854535a582484e18b0b51bb8bc28ed5c87cd1
Arsalan Shah <manactric@gmail.com>
2020-04-17 02:54:16 +0500

    1.0

---
+++ plugins/default/postbg/item.php
@@ -0,0 +1,24 @@
+<?php 
+if(isset($params['post']->postbackground_type) && !empty($params['post']->postbackground_type) && strlen($params['text']) <= 125){
+		$id = str_replace('pbg', '', $params['post']->postbackground_type);
+		$url = ossn_site_url("components/PostBackground/images/{$id}.jpg");
+?>
+<script>
+	$(document).ready(function(){
+			$element = $('.ossn-wall-item-<?php echo $params['post']->guid;?>');						   
+			if($element.length){
+					if($element.find('p:first').length){
+						$element.find('p:first').addClass('postbg-container');
+						$element.find('p:first').addClass('postbg-text');
+						
+						$element.find('p:first').css({
+							'background': 'url("<?php echo $url;?>")',
+							'background-position': 'center',
+							'background-size': 'cover',
+						});				
+
+					}
+			}
+	});
+</script>
+<?php } 
---
+++ plugins/default/postbg/js.php
@@ -0,0 +1,70 @@
+//<script>
+$(document).ready(function(){
+		var $listsbg = <?php echo json_encode(__PostBackground_List__);?>;
+		if($('.ossn-wall-container-data').length){
+				$('<div id="ossn-wall-postbg" style="display:none;"></div>').insertAfter('.ossn-wall-container-data textarea');
+				$.each($listsbg, function(){
+					$('#ossn-wall-postbg').append('<span class="" data-postbg-type="'+this['name']+'" style="background:url(\''+this['url']+'\'";background-position: center; background-size: cover;"></div>');
+				});
+				$('#ossn-wall-form').append('<input class="postbg-input" name="postbackground_type" type="hidden"/>');
+		}
+		$('body').on('click', '.ossn-wall-container-control-menu-postbg-selector', function(){
+				$('.ossn-wall-container-data div').each(function(){
+						$id = $(this).attr('id');
+						if($id && $id.indexOf('ossn-wall-') >= 0){
+								$(this).hide();
+						}	
+				});
+				if($('#ossn-wall-postbg').attr('data-toggle') == 0 || !$('#ossn-wall-postbg').attr('data-toggle')){
+					$('#ossn-wall-postbg').attr('data-toggle', 1);
+					$('#ossn-wall-postbg').show();
+				} else {
+					$('#ossn-wall-postbg').attr('data-toggle', 0);
+					$('#ossn-wall-postbg').hide();
+				}
+		});
+ 		$('.ossn-wall-container-data textarea').keyup(function(){
+   				var length = $.trim(this.value).length;
+				if(length > 125) {
+					$('.ossn-wall-container-data .postbg-container').attr('style', '');
+     				$('.ossn-wall-container-data textarea').removeClass('postbg-container');
+					if($('.postbg-input').length){
+						$('.postbg-input').val('');
+					}
+    			}
+		});		
+		$('body').on('click', '#ossn-wall-postbg span', function(){
+					$type = $(this).attr('data-postbg-type');	
+					var i = 0;
+					for(i=0;i<=$listsbg.length;i++){
+							if($listsbg[i]['name'] == $type){
+								$('.ossn-wall-container-data textarea').addClass('postbg-container');
+								$('.ossn-wall-container-data .postbg-container').css({
+											'background': 'url("'+$listsbg[i]['url']+'")',
+											'background-position': 'center',
+											'background-size': 'cover',
+								});
+								$('.postbg-input').val($type);
+								break;	
+							}
+					}
+		});
+		$(document).ajaxComplete(function(event, xhr, settings) {
+			var $url = settings.url;
+			$pagehandler = $url.replace(Ossn.site_url, '');
+			if($pagehandler.indexOf('action/wall/post/a') >= 0){
+					$('.ossn-wall-container-data .postbg-container').attr('style', '');
+     				$('.ossn-wall-container-data textarea').removeClass('postbg-container');
+					if($('.postbg-input').length){
+						$('.postbg-input').val('');
+					}
+					//hide panel
+					$('.ossn-wall-container-data div').each(function(){
+						$id = $(this).attr('id');
+						if($id && $id.indexOf('ossn-wall-') >= 0){
+								$(this).hide();
+						}
+					});					
+			}
+	});		
+});
